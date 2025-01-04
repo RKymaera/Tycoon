96036b3f993c4fa8dc5b582a07c35d3f47a944e4
Amaya Raphaelson <rkymaera@gmail.com>
2025-01-04 14:59:48 -0500

    Add proper overrides to the cards

--- Assets/Apps/Cards/Scripts/PlayingCard.cs
+++ Assets/Apps/Cards/Scripts/PlayingCard.cs
@@ -5,12 +5,11 @@ namespace Apps.Cards
 {
 public class PlayingCard : ScriptableObject
 {
+#region Enums
+    // Sorted by value
     public enum Ranks
     {
-        Joker = 0,
-        Ace,
-        Two,
-        Three,
+        Three = 3,
         Four,
         Five,
         Six,
@@ -21,6 +20,38 @@ public class PlayingCard : ScriptableObject
         Jack,
         Queen,
         King,
+        Ace,
+        Two,
+        Joker,
+    }
+
+    public static string ToString(Ranks rank)
+    {
+        switch(rank)
+        {
+            case Ranks.Three:
+            case Ranks.Four:
+            case Ranks.Five:
+            case Ranks.Six:
+            case Ranks.Seven:
+            case Ranks.Eight:
+            case Ranks.Nine:
+            case Ranks.Ten:
+                return ((int)rank).ToString();
+            case Ranks.Jack:
+                return "J";
+            case Ranks.Queen:
+                return "Q";
+            case Ranks.King:
+                return "K";
+            case Ranks.Ace:
+                return "A";
+            case Ranks.Two:
+                return "2";
+            case Ranks.Joker:
+                return "Joker";
+        }
+        return null;
     }
 
     public enum Suits
@@ -32,13 +63,56 @@ public class PlayingCard : ScriptableObject
         Spades,
     }
 
+    public static string ToString(Suits suit)
+    {
+        switch(suit)
+        {
+            case Suits.Clubs:
+                return "♣";
+            case Suits.Diamonds:
+                return "♦";
+            case Suits.Hearts:
+                return "♥";
+            case Suits.Spades:
+                return "♠";
+            case Suits.Joker:
+                return "Joker";
+        }
+        return null;
+    }
+#endregion
+
+
     public Ranks Rank = Ranks.Joker;
     public Suits Suit = Suits.Joker;
     public bool IsPlayed = false;
 
+
+#region Operators
+    // Equivalence operators
+    // For the purposes of this game, we will only be comparing the rank of the cards for equivalence operators
+    public static bool operator ==(PlayingCard a, PlayingCard b) => a.Rank == b.Rank;
+    public static bool operator !=(PlayingCard a, PlayingCard b) => a.Rank != b.Rank;
+
+    // Equals operator is overridden to reflect the Rank and Suit of the card
+    public override bool Equals(object obj) => obj is PlayingCard card && card.Rank == Rank && card.Suit == Suit;
+
+
+    // Sort operators
+    // The ranks are sorted by card value for this game, when other games are implemented, this may change
+    public static bool operator <(PlayingCard a, PlayingCard b) => a.Rank < b.Rank;
+    public static bool operator >(PlayingCard a, PlayingCard b) => a.Rank > b.Rank;
+
+    // Hash code is overridden to reflect the Rank and Suit of the card
+    public override int GetHashCode() => (int)Rank * 100 + (int)Suit;
+
+
     public static string ToString(PlayingCard card)
     {
-        return card.Rank.ToString() + " of " + card.Suit.ToString();
+        if (card.Rank == Ranks.Joker)
+            return "Joker";
+        return ToString(card.Rank) + ToString(card.Suit);
     }
+#endregion
 }
 }
